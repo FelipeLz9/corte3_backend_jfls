db412b5e1778a6f2d0e30c882688e125990fcbe5
Felipe <juanf.lopezs@autonoma.edu.co>
2024-05-23 22:11:52 -0500

    Actualizaion al backend, se añadieron nuevos modelos en el schema, se creo el controlador de usuario y sus rutas y se realizó el crud de usuario completo

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./src/routes/UserRoutes');
 
-async function main() {
-    console.log('Creating a new user mg...');
-}
+const app = express();
+app.use(bodyParser.json());
 
-main()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// RUTAS
+app.use('/users', userRoutes);
+app.use('/uploads', express.static('uploads'));
+require('dotenv').config();
+
+// conexión con la base de datos
+const CONNECTION_PORT = process.env.PORT || 3002;
+app.listen(CONNECTION_PORT, () => {
+    console.log(`Server running on port ${CONNECTION_PORT}`);
+});
